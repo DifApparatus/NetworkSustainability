4feac387dc84d75483534683ffe13baa5033a9d1
Alexandr Shapkin <shap1kin2@gmail.com>
2020-05-14 11:09:13 +0300

    Revert "Deleted WsnUdpApp based on UdpBasicBurst"
    This reverts commit ff5ceb57f60b0bde75c9f9163d6bbd78a791c262.

---
+++ src/WsnUdpApp.h
@@ -0,0 +1,46 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef WSNUDPAPP_H_
+#define WSNUDPAPP_H_
+
+#include "inet/applications/udpapp/UdpBasicBurst.h"
+#include <vector>
+
+namespace inet{
+
+class WsnUdpApp : public UdpBasicBurst {
+    std::vector<int> neighbours_Id;
+    std::vector<int> distances;
+    int problemNode;
+    int problemDistance;
+    UdpSocket broadcastSocket;
+    int maxDistance;
+    int optimalDistance;
+    int correctingDistance;
+    protected:
+        void initialize(int stage) override;
+        template<class T> Packet *createPacket(char packetName[], T payload);
+        inet::Ptr<CurrentCoordsMessage> createCoordPayload();
+        virtual void generateBurst() override;
+     //  virtual void processPacket(Packet *pk) override;
+     //  void processResiliencePacket(Packet* &pk);
+     //  virtual void processStart() override;
+     //   void sendBroadcastCoords();
+     //   void sendBroadcastCoordsReply(int destModuleId);
+     //   void sendNewConnectionRequest(L3Address addr);
+    };
+}// namespace inet
+#endif /* WSNUDPAPP_H_ */
